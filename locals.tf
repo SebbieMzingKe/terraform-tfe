252dbb374407510306443bcecefd0a3b31892774
SebbieMzingKe <sebbievilar2@gmail.com>
2025-10-17 09:45:41 +0300

    feat: set up github workspace

--- locals.tf
+++ locals.tf
@@ -11,5 +11,11 @@ locals {
       project_id     = module.project["cloud-infrastructure-tfe"].id
       vcs_repo_identifier = "${var.github_organization_name}/terraform-tfe"
     }
+    "cloud-infrastructure-github" = {
+      description = "example automation workspace for github resources"
+      execution_mode = "local"
+      project_id = module.project["cloud-infrastructure-tfe"].id
+      vcs_repo_identifier = "${var.github_organization_name}/cloud-infrastructure-github"
+    }
   }
 }
