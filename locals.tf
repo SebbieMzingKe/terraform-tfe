e4cdc30690b83098999cfa0ed777394d61b26fb5
SebbieMzingKe <sebbievilar2@gmail.com>
2025-10-22 17:13:50 +0300

    feat: add workspaces.

--- locals.tf
+++ locals.tf
@@ -6,16 +6,73 @@ locals {
   }
   workspace = {
     "cloud-infrastructure-tfe" = {
-      description    = "example description workspace"
-      execution_mode = "remote"
-      project_id     = module.project["cloud-infrastructure-tfe"].id
+      description         = "example description workspace"
+      execution_mode      = "remote"
+      project_id          = module.project["cloud-infrastructure-tfe"].id
       vcs_repo_identifier = "${var.github_organization_name}/terraform-tfe"
     }
     "cloud-infrastructure-github" = {
-      description = "example automation workspace for github resources"
-      execution_mode = "remote"
-      project_id = module.project["cloud-infrastructure-tfe"].id
+      description         = "example automation workspace for github resources"
+      execution_mode      = "remote"
+      project_id          = module.project["cloud-infrastructure-tfe"].id
       vcs_repo_identifier = "${var.github_organization_name}/cloud-infrastructure-github"
+    }
+
+    "cloud-infrastructure-aws-network" = {
+      description         = "automation workspace for network resources"
+      execution_mode      = "remote"
+      project_id          = module.project["cloud-infrastructure-tfe"].id
+      vcs_repo_identifier = "${var.github_organization_name}/cloud-infrastructure-aws-network"
+
+      variable = [
+        {
+          category = "terraform"
+          hcl      = true
+          key      = "azs"
+          value    = jsonencode(["us-west-2a", "us-west-2b"])
+        },
+        {
+          category = "terraform"
+          key      = "cidr"
+          value    = "10.0.0.0/16"
+        },
+        {
+          category = "terraform"
+          key      = "name"
+          value    = "enterprise-cloud-infra"
+        },
+      ]
+    }
+   
+   
+    "cloud-infrastructure-aws-cluster-prod" = {
+      description         = "automation workspace for cluster resources"
+      execution_mode      = "remote"
+      project_id          = module.project["cloud-infrastructure-tfe"].id
+      vcs_repo_identifier = "${var.github_organization_name}/cloud-infrastructure-aws-cluster"
+
+      variable = [
+        {
+          category = "terraform"
+          key      = "domain"
+          value    = "muyoma.space"
+        },
+        {
+          category = "terraform"
+          key      = "environment"
+          value    = "prod"
+        },
+        {
+          category = "terraform"
+          key      = "name"
+          value    = "enterprise-cloud-infra-seb"
+        },
+        {
+          category = "terraform"
+          key      = "vpc_name"
+          value    = "enterprise-cloud-infra"
+        },
+      ]
     }
   }
 }
